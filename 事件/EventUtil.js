29f2a6d1ad6c9775b72a4884dcbaa00c612f371f
jackwang <wdmzjjm@163.com>
2016-12-31 22:24:25 +0800

    P388

--- 事件/EventUtil.js
+++ 事件/EventUtil.js
@@ -44,12 +44,58 @@ var EventUtil = {
         }else{
             event.cancelBubble = true;
         }
+    },
+
+    //对于mouseover和mouseout才包含值
+    //获取相关元素
+    getRelatedTarget: function(event){
+        if(event.relatedTarget){
+            return event.relatedTarget;
+        }else if(event.toElement){
+            return event.toElement;
+        }else if(event.fromElement){
+            return event.fromElement;
+        }else{
+            return null;
+        }
+    },
+
+    //获取鼠标按键
+    getButton: function(event){
+        if(document.implementation.hasFeature("MouseEvents", "2.0")){
+            return event.button;
+        }else{
+            switch(event.button){
+                case 0:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    return 0;
+                case 2:
+                case 6:
+                    return 2;
+                case 4:
+                    return 1;
+            }
+        }
+    },
+
+    //获取鼠标滚轮增量值
+    getWheelDelta: function(event){
+        if(event.wheelDelta){
+            return (client.engine.opera && client.engine.opera < 9.5) ? -event.wheelDelta : event.wheelDelta;
+        }else{
+            return -event.detail * 40;
+        }
+    },
+
+    //获取字符编码
+    getCharCode: function(event){
+        if(typeof event.charCode == "number"){
+            return event.charCode;
+        }else{
+            return event.keyCode;
+        }
     }
 };
-
-var btn = document.getElementById("myBtn");
-var handler = function(){
-    console.log("clicked");
-};
-EventUtil.addHandler(btn, "click", handler);
-// EventUtil.removeHandler(btn, "click", handler);
