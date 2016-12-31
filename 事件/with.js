255d7485f0782f230fce6adf1f54d67262bc089c
jackwang <wdmzjjm@163.com>
2016-12-31 10:14:52 +0800

    P362

---
+++ 事件/with.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jackwang on 2016/12/31.
+ */
+({
+    x: 10,
+    foo: function () {
+        function bar() {
+            console.log(x);
+            console.log(y);
+            console.log(this.x);
+        }
+        with (this) {
+            var x = 20;
+            var y = 30;
+            bar.call(this);
+        }
+    }
+}).foo();
